e6cfc8361e9fdb9a4fdbad52c8782e8b22457ad8
Amir Fahmideh <amir.fahmideh@hotmail.com>
2021-04-13 12:11:28 +0430

    Add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,51 @@
 import React from "react";
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
 import "./app.css";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    margin: 0,
+    padding: 0,
+  },
+  menuButton: {},
+  title: {
+    flexGrow: 1,
+    marginRight: theme.spacing(2),
+  },
+}));
+
 export const App: React.FunctionComponent<{}> = () => {
-  return <div>Welcome to default application!</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Today
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,15 @@
 import React from "react";
 import dom from "react-dom";
+import "./index.css";
 import App from "./app";
-dom.render(<App />, document.getElementById("root"));
+
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { faIR } from "@material-ui/core/locale";
+
+const theme = createMuiTheme({}, faIR);
+dom.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
