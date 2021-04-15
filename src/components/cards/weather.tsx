323421e0cc15bf850703100eaceaf4bb47dd45e9
Amir Fahmideh <amir.fahmideh@hotmail.com>
2021-04-15 13:50:40 +0430

    Add more items to weather info

--- src/components/cards/weather.tsx
+++ src/components/cards/weather.tsx
@@ -1,13 +1,19 @@
 import {
+  Badge,
   Card,
+  CardActions,
   CardContent,
-  CardHeader,
+  Collapse,
   Grid,
+  IconButton,
   makeStyles,
+  Typography,
 } from "@material-ui/core";
 import React from "react";
 import { getWeatherResult } from "../../api/openWeatherApi";
 import { WeatherResult } from "../../api/weatherResult";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme) => ({
   card: {},
@@ -19,14 +25,28 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: theme.typography.fontFamily,
   },
   degree: {
-    fontSize: theme.typography.h2.fontSize,
+    fontSize: theme.typography.h3.fontSize,
   },
   minMax: {
     fontSize: theme.typography.h6.fontSize,
   },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
 }));
 export const Weather: React.FunctionComponent<{}> = () => {
   const [weatherResult, setWeatherResult] = React.useState<WeatherResult>();
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   const classes = useStyles();
 
   React.useEffect(() => {
@@ -38,9 +58,9 @@ export const Weather: React.FunctionComponent<{}> = () => {
 
   return (
     <Card className={classes.card}>
-      <CardHeader className={classes.cardHeader} title="آب و هوا"></CardHeader>
+      {/* <CardHeader className={classes.cardHeader} title="آب و هوا"></CardHeader> */}
       <CardContent>
-        <Grid container>
+        <Grid container spacing={0}>
           <Grid item xs={12} className={classes.gridItem}>
             {weatherResult && (
               <div>
@@ -65,6 +85,48 @@ export const Weather: React.FunctionComponent<{}> = () => {
             {weatherResult && <p>{weatherResult.weather[0].description}</p>}
           </Grid>
         </Grid>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="بیشتر"
+        >
+          <ExpandMoreIcon></ExpandMoreIcon>
+        </IconButton>
+      </CardActions>
+      <CardContent>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <Grid container spacing={6}>
+            <Grid item className={classes.gridItem}>
+              <Typography>سرعت باد</Typography>
+              <Typography>
+                <b>{weatherResult?.wind.speed}</b>
+              </Typography>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+              <Typography>درجه باد</Typography>
+              <Typography>
+                <b>{weatherResult?.wind.deg}</b>
+              </Typography>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+              <Typography>رطوبت</Typography>
+              <Typography>
+                <b>{weatherResult?.main.humidity}</b>
+              </Typography>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+              <Typography>فشار</Typography>
+              <Typography>
+                <b>{weatherResult?.main.pressure}</b>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Collapse>
       </CardContent>
     </Card>
   );
