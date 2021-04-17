a9a9e0926ffb4226ba9f2b24984be46365fd3a88
Amir Fahmideh <amir.fahmideh@hotmail.com>
2021-04-17 11:34:01 +0430

    change http to https update github pages

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Weather
 
 Showing weather in current city for Today!
+
+## Preview on Github Page
+
+[https://amirfahmideh.github.io/today/](https://amirfahmideh.github.io/today/)
--- src/api/openWeatherApi.ts
+++ src/api/openWeatherApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { WeatherResult } from "./weatherResult";
 export const getWeatherResult = (): Promise<WeatherResult> => {
   const api_key = "2f6ba57fc56bca4b09deabe261e6064e";
-  var api_url = `http://api.openweathermap.org/data/2.5/weather?q=mashhad&appid=${api_key}&units=metric`;
+  var api_url = `https://api.openweathermap.org/data/2.5/weather?q=mashhad&appid=${api_key}&units=metric`;
   return axios
     .get(api_url)
     .then(({ data }) => {
